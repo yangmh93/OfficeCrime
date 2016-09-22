dbfab29569b19c56779a1c31825befd97dd31a20
Michael Yang <yangmh@appstate.edu>
2016-09-22 10:06:09 -0400

    abstract activity

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,13 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by Michael on 9/22/2016.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Michael on 9/22/2016.
+ */
+public class CrimeListFragment extends Fragment {
+    //nothing yet
+}
